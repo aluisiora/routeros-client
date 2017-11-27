8e6b3c5dbe9454382a544a26ea22c18fc37bee6f
Aluísio Amaral <aluisioramtl@gmail.com>
2017-11-27 10:50:24 -0200

    edit as alias of update

--- src/RosApiCrud.ts
+++ src/RosApiCrud.ts
@@ -161,6 +161,16 @@ export abstract class RouterOSAPICrud {
     }
 
     /**
+     * Alias of update
+     * 
+     * @param data the new data to update the item
+     * @param ids optional id(s) of the rules
+     */
+    public edit(data: object, ids?: Types.Id): Types.SocPromise {
+        return this.update(data, ids);
+    }
+
+    /**
      * Creates the full array of sentences that will be
      * compatible with the raw API to be sent to the
      * routerboard using all the functions triggered
