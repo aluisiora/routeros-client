b4a4c5d2e1939d631f71f668cf23de3c69ea8081
Aluísio Amaral <aluisioramtl@gmail.com>
2019-01-07 09:29:27 -0200

    Added prettier and rimraf

--- test/4.streaming.spec.js
+++ test/4.streaming.spec.js
@@ -1,60 +1,63 @@
-const RouterOSClient = require("../dist").RouterOSClient;
-const chai = require("chai");
-const config = require("./config.json");
+const RouterOSClient = require('../dist').RouterOSClient;
+const chai = require('chai');
+const config = require('./config.json');
 
 const should = chai.should();
 
 let conn, api;
 
-describe("RosApiCommands", () => {
-
-    before("should stablish connection and save api object", (done) => {
+describe('RosApiCommands', () => {
+    before('should stablish connection and save api object', done => {
         conn = new RouterOSClient({
             host: config.host,
             user: config.user,
             password: config.password,
-            keepalive: true
+            keepalive: true,
         });
-        conn.connect().then((connApi) => {
-            api = connApi;
-            done();
-        }).catch((err) => {
-            done(err);
-        });
+        conn.connect()
+            .then(connApi => {
+                api = connApi;
+                done();
+            })
+            .catch(err => {
+                done(err);
+            });
     });
 
-    describe("streaming content", function() {
-        this.timeout(8000);
-        
-        it("should stream torch on interface 1 and stop after 5 seconds", (done) => {
+    describe('streaming content', function() {
+        this.timeout(12000);
 
-            const menu = api.menu("/tool/torch");
+        it('should stream torch on interface 1 and stop after 5 seconds', done => {
+            const menu = api.menu('/tool/torch');
             let started = false;
-            menu.where("interface", "ether1").stream((err, packet, torch) => {
+            menu.where('interface', 'ether1').stream((err, packet, torch) => {
                 should.not.exist(err);
                 should.exist(packet);
 
                 if (!started) {
                     started = true;
                     setTimeout(() => {
-                        torch.stop().then(() => {
-                            done();
-                        }).catch((err) => {
-                            done(err);
-                        });
+                        torch
+                            .stop()
+                            .then(() => {
+                                done();
+                            })
+                            .catch(err => {
+                                done(err);
+                            });
                     }, 5000);
-                }                
+                }
             });
         });
-
     });
 
-    after("should disconnect", (done) => {
-        conn.close().then(() => {
-            done();
-        }).catch((err) => {
-            done(err);
-        });
+    after('should disconnect', done => {
+        conn.close()
+            .then(() => {
+                done();
+            })
+            .catch(err => {
+                done(err);
+            });
     });
-
 });
